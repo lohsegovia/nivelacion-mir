4c7e6013dfe9dcabe19a474961851f77a6ebcb32
Lorena <lorenasegovia@outlook.com>
2022-11-23 22:46:23 -0300

    agrega

---
+++ ejercicio6/index.js
@@ -0,0 +1,22 @@
+//paso
+const pedro = {
+    nombre: "Pedro Perez",
+    edad: 30,
+    activo: true,
+    hobbies: ["programar", "squash", "piano"]       
+  }
+  // paso5
+  //console.log("La edad es:" +pedro.edad);
+  //paso 6
+  pedro.estatura= 1.8
+  //paso7
+  delete pedro.activo
+  //paso8
+  //for (const property in pedro){
+    //console.log(`${property}: ${pedro[property]}`)
+  //}
+  //paso9
+  function saluda(){
+    console.log("Hola, me llamo "+pedro.nombre)
+  } return saluda();
+  
